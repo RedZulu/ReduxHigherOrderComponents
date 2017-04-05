d4209df5eb68aaa1c8218bd7f3c12063e3e2d052
Ryan Zell <rzell@EUCLID2003.local>
2017-04-05 10:23:06 -0400

    add dummy authentication

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>1 Cup Salt</li>
+        <li>1 Cup Butter</li>
+        <li>1 Cup Pickles</li>
+      </ul>
+    </div>
+  );
+}
